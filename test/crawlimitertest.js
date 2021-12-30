4b3eedd08a40d033a081e15388325b3a4668de2b
unknown <conorpaulcrowe@gmail.com>
2021-12-31 02:40:42 +1000

    Cleaned up

--- test/crawlimitertest.js
+++ test/crawlimitertest.js
@@ -1,3 +1,6 @@
+/*
+This is a massive todo. QA engineer didnt bother to automate testing of his own code? Surely not...
+*/
 ////test can be created
 //const crawler = new Crawlimiter([
 //   {requestLimit: 20, timeInterval: 6, initialQuota: 20},
@@ -19,3 +22,19 @@
 //}
 //console.log(crawler.queue);
 //setTimeout(() => {crawler.stop()}, 5000) //TODO gentle stop to empty queue first
+
+
+////helper tests
+// console.log(getQuotaIntervals(
+//     [
+//         {requestLimit: 20, timeInterval: 5},
+//         {requestLimit: 100, timeInterval: 60, initialQuota: 50},
+//     ]
+// ));
+// console.log(getQuotaIntervals({requestLimit: 100, timeInterval: 60, initialQuota: 20}));
+// console.log(getQuotaIntervals([{}]));
+// console.log(getQuotaIntervals([]));
+
+// const test = {requestLimit: 100, timeInterval: 60};
+// console.log(getQuotaIntervals(test));
+// console.log(test);
